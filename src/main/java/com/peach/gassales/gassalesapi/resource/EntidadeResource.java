06de0386a3dd689b009b49587cb9160b95230b8c
bcassamo <banucho.cassamo@gmail.com>
2021-06-07 22:11:29 +0200

    Crud Entidade

--- src/main/java/com/peach/gassales/gassalesapi/resource/EntidadeResource.java
+++ src/main/java/com/peach/gassales/gassalesapi/resource/EntidadeResource.java
@@ -1,6 +1,7 @@
 package com.peach.gassales.gassalesapi.resource;
 
 import com.peach.gassales.gassalesapi.event.RecursoCriadoEvent;
+import com.peach.gassales.gassalesapi.model.Endereco;
 import com.peach.gassales.gassalesapi.model.Entidade;
 import com.peach.gassales.gassalesapi.repository.EntidadeRepository;
 import com.peach.gassales.gassalesapi.service.EntidadeService;
@@ -40,9 +41,40 @@ public class EntidadeResource {
     @GetMapping
     public List<Entidade> listar() { return entidadeRepository.findAll(); }
 
+    @PostMapping
+    public ResponseEntity<Entidade> criarEntidade(@Valid @RequestBody Entidade entidade, HttpServletResponse response) {
+        Entidade entidadeSalvo = entidadeRepository.save(entidade);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, entidadeSalvo.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(entidadeSalvo);
+    }
+
     @GetMapping("/clientes")
     public List<Entidade> listarClientes() {
         return entidadeService.buscarTodosClientes();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Entidade> buscarClientePeloCodigo(@PathVariable Long id) {
+        Optional<Entidade> cliente = entidadeService.buscarClientePeloCodigo(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerCliente(@PathVariable Long id) {
+        entidadeService.removerCliente(id);
+    }
+
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<Entidade> actualizarCliente(@PathVariable Long id, @Valid @RequestBody Entidade entidade) {
+        Entidade clienteSalvo = entidadeService.actualizarCliente(id, entidade);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @PutMapping("/clientes/{id}/endereco")
+    public ResponseEntity<Entidade> actualizarEnderecoDoCliente(@PathVariable Long id, @Valid @RequestBody Endereco endereco) {
+        Entidade clienteSalvo = entidadeService.actualizarEnderecoDoCliente(id, endereco);
+        return ResponseEntity.ok(clienteSalvo);
     }
 
     @GetMapping("/fornecedores")
@@ -50,27 +82,27 @@ public class EntidadeResource {
         return entidadeService.buscarTodosFornecedores();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Entidade> buscarClientePeloCodigo(@PathVariable Long id) {
-        Optional<Entidade> cliente = entidadeService.buscarClientePeloCodigo(id);
-        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/fornecedores/{id}")
+    public ResponseEntity<Entidade> buscarFornecedorPeloCodigo(@PathVariable Long id) {
+        Optional<Entidade> fornecedor = entidadeService.buscarFornecedorPeloCodigo(id);
+        return fornecedor.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
-    public void remover(@PathVariable Long id) {
-        entidadeRepository.deleteById(id);
+    @DeleteMapping("/fornecedores/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerFornecedor(@PathVariable Long id) {
+        entidadeService.removerFornecedor(id);
     }
 
-    @PostMapping
-    public ResponseEntity<Entidade> criar(@Valid @RequestBody Entidade entidade, HttpServletResponse response) {
-        Entidade entidadeSalvo = entidadeRepository.save(entidade);
-        publisher.publishEvent(new RecursoCriadoEvent(this, response, entidadeSalvo.getId()));
-        return ResponseEntity.status(HttpStatus.CREATED).body(entidadeSalvo);
+    @PutMapping("/fornecedores/{id}")
+    public ResponseEntity<Entidade> actualizarFornecedor(@PathVariable Long id, @Valid @RequestBody Entidade entidade) {
+        Entidade fornecedorSalvo = entidadeService.actualizarFornecedor(id, entidade);
+        return ResponseEntity.ok(fornecedorSalvo);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Entidade> actualizar(@PathVariable Long id, @Valid @RequestBody Entidade entidade) {
-        Entidade entidadeSalvo = entidadeService.actualizar(id, entidade);
-        return ResponseEntity.ok(entidadeSalvo);
+    @PutMapping("/fornecedores/{id}/endereco")
+    public ResponseEntity<Entidade> actualizarEnderecoDoFornecedor(@PathVariable Long id, @Valid @RequestBody Endereco endereco) {
+        Entidade clienteSalvo = entidadeService.actualizarEnderecoDoFornecedor(id, endereco);
+        return ResponseEntity.ok(clienteSalvo);
     }
 }
--- src/main/java/com/peach/gassales/gassalesapi/service/EntidadeService.java
+++ src/main/java/com/peach/gassales/gassalesapi/service/EntidadeService.java
@@ -1,15 +1,16 @@
 package com.peach.gassales.gassalesapi.service;
 
+import com.peach.gassales.gassalesapi.model.Endereco;
 import com.peach.gassales.gassalesapi.model.Entidade;
 import com.peach.gassales.gassalesapi.model.TipoEntidade;
 import com.peach.gassales.gassalesapi.repository.EntidadeRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,11 +18,8 @@ import java.util.Optional;
 public class EntidadeService {
     private EntidadeRepository entidadeRepository;
 
-    List<Entidade> clientes;
-    List<Entidade> fornecedores;
-
     @Autowired
-    private void setClienteRepository(EntidadeRepository entidadeRepository) {
+    private void setEntidadeRepository(EntidadeRepository entidadeRepository) {
         this.entidadeRepository = entidadeRepository;
     }
 
@@ -32,16 +30,53 @@ public class EntidadeService {
      * @throws EmptyResultDataAccessException se não encontrar cliente com o id fornecido
      */
     public Optional<Entidade> buscarClientePeloCodigo(Long id) {
-        Optional<Entidade> cliente = entidadeRepository.findById(id);
+        Optional<Entidade> cliente = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.CLIENTE, id);
         if (cliente.isEmpty())
                 throw new EmptyResultDataAccessException(1);
         return cliente;
     }
 
-    public Entidade actualizar(Long id, Entidade entidade) {
-        Entidade entidadeSalvo = entidadeRepository.findById(id).get();
-        BeanUtils.copyProperties(entidade, entidadeSalvo, "id");
-        return entidadeRepository.save(entidadeSalvo);
+    public Optional<Entidade> buscarFornecedorPeloCodigo(Long id) {
+        Optional<Entidade> fornecedor = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.FORNECEDOR, id);
+        if (fornecedor.isEmpty())
+            throw new EmptyResultDataAccessException(1);
+        return fornecedor;
+    }
+
+    public Entidade actualizarCliente(Long id, Entidade entidade) {
+        Entidade clienteSalvo = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.CLIENTE, id).get();
+        if(entidade.getTipo().equals(TipoEntidade.FORNECEDOR))
+            throw new DataIntegrityViolationException("Esta tentar actualizar um cliente");
+
+        BeanUtils.copyProperties(entidade, clienteSalvo, "id");
+        return entidadeRepository.save(clienteSalvo);
+    }
+
+    public Entidade actualizarFornecedor(Long id, Entidade entidade) {
+        Entidade forncedorSalvo = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.FORNECEDOR, id).get();
+        if(entidade.getTipo().equals(TipoEntidade.CLIENTE))
+            throw new DataIntegrityViolationException("Esta tentar actualizar um fornecedor");
+
+        BeanUtils.copyProperties(entidade, forncedorSalvo, "id");
+        return entidadeRepository.save(forncedorSalvo);
+    }
+
+    public Entidade actualizarEnderecoDoCliente(Long id, Endereco endereco) {
+        Entidade clienteSalvo = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.CLIENTE, id).get();
+        if(clienteSalvo.getTipo().equals(TipoEntidade.FORNECEDOR))
+            throw new DataIntegrityViolationException("Esta tentar actualizar endereco de um cliente");
+
+        clienteSalvo.setEndereco(endereco);
+        return entidadeRepository.save(clienteSalvo);
+    }
+
+    public Entidade actualizarEnderecoDoFornecedor(Long id, Endereco endereco) {
+        Entidade fornecedorSalvo = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.FORNECEDOR, id).get();
+        if(fornecedorSalvo.getTipo().equals(TipoEntidade.CLIENTE))
+            throw new DataIntegrityViolationException("Esta tentar actualizar endereco de um fornecedor");
+
+        fornecedorSalvo.setEndereco(endereco);
+        return entidadeRepository.save(fornecedorSalvo);
     }
 
     public List<Entidade> buscarTodosClientes() {
@@ -52,14 +87,25 @@ public class EntidadeService {
         return seleccionarEntidades(TipoEntidade.FORNECEDOR);
     }
 
+    @Transactional
+    public Optional<Entidade> removerCliente(Long id) {
+        Optional<Entidade> cliente = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.CLIENTE, id);
+        if (cliente.isEmpty())
+            throw new EmptyResultDataAccessException(1);
+        entidadeRepository.deleteByTipoAndId(TipoEntidade.CLIENTE, id);
+        return cliente;
+    }
+
+    @Transactional
+    public Optional<Entidade> removerFornecedor(Long id) {
+        Optional<Entidade> fornecedor = entidadeRepository.findEntidadeByTipoAndId(TipoEntidade.FORNECEDOR, id);
+        if (fornecedor.isEmpty())
+            throw new EmptyResultDataAccessException(1);
+        entidadeRepository.deleteByTipoAndId(TipoEntidade.FORNECEDOR, id);
+        return fornecedor;
+    }
+
     private List<Entidade> seleccionarEntidades(TipoEntidade tipoEntidade) {
-        List<Entidade> entidadesSalvas = entidadeRepository.findAll();
-        List<Entidade> entidades = new ArrayList<>();
-        for (Entidade entidade: entidadesSalvas) {
-            if (tipoEntidade.equals(entidade.getTipo())) {
-                entidades.add(entidade);
-            }
-        }
-        return entidades;
+        return entidadeRepository.findAllByTipo(tipoEntidade);
     }
 }
