597438228892ec5e1d95a60009bb98224343f639
bcassamo <banucho.cassamo@gmail.com>
2022-01-29 20:36:26 +0200

    Refactoramento de Lancamento e de Business

--- src/main/resources/db/migration/V007__criar_e_registar_business.sql
+++ src/main/resources/db/migration/V007__criar_e_registar_business.sql
@@ -4,9 +4,11 @@ CREATE TABLE business (
     descricao VARCHAR(20) NOT NULL,
     data_business DATE NOT NULL,
     id_lancamento BIGINT NOT NULL,
-    FOREIGN KEY (id_lancamento) REFERENCES lancamento(id)
+    id_entidade BIGINT NOT NULL,
+    FOREIGN KEY (id_lancamento) REFERENCES lancamento(id),
+    FOREIGN KEY (id_entidade) REFERENCES entidade(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO business (codigo_business, descricao, data_business, id_lancamento) values ('20210608-001A', 'Aquisição', '2021-06-08', 1);
-INSERT INTO business (codigo_business, descricao, data_business, id_lancamento) values ('20210608-001A', 'Aquisição', '2021-06-08', 2);
-INSERT INTO business (codigo_business, descricao, data_business, id_lancamento) values ('20210608-002V','Venda', '2021-06-08', 3);
+INSERT INTO business (codigo_business, descricao, data_business, id_lancamento, id_entidade) values ('20210608-001A', 'Aquisição', '2021-06-08', 1, 3);
+INSERT INTO business (codigo_business, descricao, data_business, id_lancamento, id_entidade) values ('20210608-001A', 'Aquisição', '2021-06-08', 2, 3);
+INSERT INTO business (codigo_business, descricao, data_business, id_lancamento, id_entidade) values ('20210608-002V','Venda', '2021-06-08', 3, 1);
