4c022349f3d9febd3142bc004b11cd78ac6f9a89
bcassamo <banucho.cassamo@gmail.com>
2022-05-03 19:33:32 +0200

    fixando origem permitida

--- src/main/java/com/peach/gassales/gassalesapi/config/property/GassalesProperty.java
+++ src/main/java/com/peach/gassales/gassalesapi/config/property/GassalesProperty.java
@@ -3,11 +3,10 @@ package com.peach.gassales.gassalesapi.config.property;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @ConfigurationProperties("gassales")
 @Getter @Setter
 public class GassalesProperty {
-    private  String originPermitida = "https://peachgassales-api.herokuapp.com";
+    private  String originPermitida = "https://peachgassales-angular.herokuapp.com";
 
 }
